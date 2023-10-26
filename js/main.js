fb9e6e2758b4fb5969f024ca9ed54aa72c7ba28a
KIMKYEONGMUN <139826895+KIMKYEONGMUN@users.noreply.github.com>
2023-10-26 16:29:52 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    var swiper = new Swiper(".swiperCont1", {
+    var swiper1 = new Swiper(".swiperCont1", {
         pagination: {
           el: ".swiper-pagination-cont1",
           clickable: true,
@@ -8,7 +8,7 @@ $(function(){
         autoplay: true,
       });
 
-    var swiper = new Swiper(".swiperCont2", {
+    var swiper2 = new Swiper(".swiperCont2", {
         slidesPerView: 3,
         spaceBetween: 20,
         pagination: {
@@ -16,5 +16,16 @@ $(function(){
           clickable: true,
         },
       });
+
+    $('.pause').click(function(){
+      swiper1.autoplay.stop();
+      $(this).hide();
+      $('.play').show();
+    });
+    $('.play').click(function(){
+      swiper1.autoplay.start();
+      $(this).hide();
+      $('.pause').show();
+    });
 
 });// jquery end
